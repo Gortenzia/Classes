85dfcb3e5319421b05dfc6b2129e6229e44bc1f6
Gortenzia <tacumineka@gmail.com>
2022-02-05 17:38:52 +0300

    Структура готова, остались тесты

---
+++ src/js/__tests__/zombie.test.js
@@ -0,0 +1,1 @@
+import Zombie from "../zombie";
---
+++ src/js/сharacter.js
@@ -0,0 +1,20 @@
+export default class Character {
+  constructor(name = ' ', type = ' ', health = 100, level = 1) {
+    if (name.length >= 2 && name.length <= 10) {
+      this.name = name;
+      //name = /{2,10}/; 
+      //Можно ли с помощью регулярных выражений указать длинну строки в консрукторе ?
+    }
+    const types = ['Bowerman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'];
+    if (!types.includes(type)) {
+      throw new Error(`Данный вид персоонажа ${type} не существует`);
+    } else {
+      this.type = type;
+    }
+
+    this.health = health;
+    this.level = level;
+        
+  }
+}
+
